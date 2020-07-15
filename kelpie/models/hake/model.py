5838f3e4cc23b6dc1e3c0ce6c4fce49286ae2079
Luke-E <luke-e.96@hotmail.com>
2020-07-15 17:49:46 +0200

    Hotfix

--- kelpie/models/hake/model.py
+++ kelpie/models/hake/model.py
@@ -196,6 +196,8 @@ class Hake(Model, nn.Module):
 
         elif batch_type == BatchType.TAIL_BATCH:
             head_part, tail_part = sample
+            print(head_part)
+            print(tail_part)
             batch_size, negative_sample_size = tail_part.size(0), tail_part.size(1)
 
             head = torch.index_select(
