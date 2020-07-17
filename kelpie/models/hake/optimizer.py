1f386f058ab01dcacf7cec09d61c8a74eaee7c57
Luke-E <luke-e.96@hotmail.com>
2020-07-17 10:14:13 +0200

    Hotfix

--- kelpie/models/hake/optimizer.py
+++ kelpie/models/hake/optimizer.py
@@ -65,6 +65,7 @@ class HakeOptimizer:
             #with tqdm(range(actual_steps), desc='epoch #'+str(step)) as bar:
             with trange(actual_steps) as bar:
                 bar.set_description('epoch #' + str(step))
+                bar.set_postfix(loss="...")
                 for i in bar:
                     loss = self.train_step(train_iterator)
                     np.random.seed()    #resets np.random seed
