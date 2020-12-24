970a725bcbb3968df0cf83ec2023e9e16ea764d6
Andrea Rossi <andrea@MBP-di-Andrea.lan>
2020-12-24 19:11:52 +0100

    complete refactoring of engines

--- engines/post_training_engine.py
+++ engines/post_training_engine.py
@@ -38,7 +38,8 @@ class PostTrainingEngine(ExplanationEngine):
 
     def simple_removal_explanations(self,
                                     sample_to_explain: Tuple[Any, Any, Any],
-                                    perspective: str):
+                                    perspective: str,
+                                    top_k: int):
 
         # the behaviour of the engine must be deterministic!
         seed = 42
@@ -55,7 +56,8 @@ class PostTrainingEngine(ExplanationEngine):
         # create a Kelpie Dataset focused on the original id of the entity to explain
         kelpie_dataset = KelpieDataset(dataset=self.dataset, entity_id=original_entity_id)
 
-        print("### BASE POST-TRAINING")
+        print("\tEntity to explain: " + self.dataset.entity_id_2_name[original_entity_id] + " (degree: " + str(self.dataset.entity_2_degree[original_entity_id]) + ")")
+        print("\tRunning base post-training before removal...")
         post_trained_model = self.post_train(kelpie_dataset=kelpie_dataset,
                                              kelpie_train_samples=kelpie_dataset.kelpie_train_samples) # type: KelpieModel
 
@@ -70,7 +72,6 @@ class PostTrainingEngine(ExplanationEngine):
         # this is confusing!
 
         # introduce perturbation
-        print("Perturbing entities...")
         perturbed_list, skipped_list = kelpie_perturbation.perturbate_samples(kelpie_dataset.kelpie_train_samples)
 
         results = []
@@ -81,11 +82,12 @@ class PostTrainingEngine(ExplanationEngine):
             cur_skipped_sample = Dataset.replace_entity_in_sample(sample=skipped_list[i][0],
                                                                   old_entity=kelpie_dataset.kelpie_entity_id,
                                                                   new_entity=kelpie_dataset.original_entity_id)
-            print("### ITER %i" % i)
-            print("### SKIPPED SAMPLE: ")
-            print("\t" + ";".join((kelpie_dataset.entity_id_2_name[int(cur_skipped_sample[0])],
-                                   kelpie_dataset.relation_id_2_name[int(cur_skipped_sample[1])],
-                                   kelpie_dataset.entity_id_2_name[int(cur_skipped_sample[2])])))
+
+            head_name = kelpie_dataset.entity_id_2_name[int(cur_skipped_sample[0])]
+            rel_name = kelpie_dataset.relation_id_2_name[int(cur_skipped_sample[1])]
+            tail_name = kelpie_dataset.entity_id_2_name[int(cur_skipped_sample[2])]
+
+            print("\tIteration " + str(i) + ": skipping sample <"+ ", ".join([head_name, rel_name, tail_name]) + ">")
 
             cur_kelpie_model = self.post_train(kelpie_dataset=kelpie_dataset,
                                                kelpie_train_samples=cur_training_samples)
@@ -99,13 +101,86 @@ class PostTrainingEngine(ExplanationEngine):
 
         results = sorted(results, key=lambda element: element[-1], reverse=True)
 
-        return results
+        if top_k == -1 or top_k < len(results):
+            return results
+        else:
+            return results[:top_k]
 
 
     def simple_addition_explanations(self,
                     sample_to_explain: Tuple[Any, Any, Any],
                     perspective: str,
                     samples_to_add: list):
+
+        # sample to explain as a numpy array of the ids
+        head_id, relation_id, tail_id = sample_to_explain
+        # name and id of the entity to explain, based on the passed perspective
+        original_entity_id = head_id if perspective == "head" else tail_id
+
+        # create a Kelpie Dataset focused on the original id of the entity to explain
+        kelpie_dataset = KelpieDataset(dataset=self.dataset, entity_id=original_entity_id)
+
+        print("\t\tRunning base post-training before additions...")
+        base_pt_model = self.post_train(kelpie_dataset=kelpie_dataset,
+                                             kelpie_train_samples=kelpie_dataset.kelpie_train_samples) # type: KelpieModel
+
+        # check how the
+        # check how the post-trained "clone" performs on the sample to explain
+        original_target_entity_score, \
+        original_best_entity_score, \
+        original_target_entity_rank = self.extract_detailed_performances_on_sample(self.model, sample_to_explain)
+
+        base_pt_target_entity_score, \
+        base_pt_best_entity_score, \
+        base_pt_target_entity_rank = self.extract_detailed_performances_on_sample(base_pt_model, sample_to_explain)
+
+
+        added_sample_2_relevance = dict()
+        added_sample_2_pt_results = dict()
+        for i in range(len(samples_to_add)):
+            cur_sample_to_add = samples_to_add[i]
+
+            head_name = kelpie_dataset.entity_id_2_name[int(cur_sample_to_add[0])]
+            rel_name = kelpie_dataset.relation_id_2_name[int(cur_sample_to_add[1])]
+            tail_name = kelpie_dataset.entity_id_2_name[int(cur_sample_to_add[2])]
+            print("\t\tIteration " + str(i) + ": adding sample <"+ ", ".join([head_name, rel_name, tail_name]) + ">")
+
+            # create a Kelpie Dataset that also contains the fact to add
+            cur_kelpie_dataset = copy.deepcopy(kelpie_dataset)
+
+            cur_sample_to_add_numpy_arr = numpy.array([cur_sample_to_add])
+            cur_kelpie_dataset.add_training_samples(cur_sample_to_add_numpy_arr)
+
+            cur_kelpie_model = self.post_train(kelpie_dataset=kelpie_dataset,
+                                               kelpie_train_samples=cur_kelpie_dataset.kelpie_train_samples)  # type: KelpieModel
+
+            pt_target_entity_score, \
+            pt_best_entity_score, \
+            pt_target_entity_rank = self.extract_detailed_performances_on_sample(cur_kelpie_model, sample_to_explain)
+
+            # TODO: this should depend on maximize/minimize
+            direct_diff = pt_target_entity_score - base_pt_target_entity_score
+
+            added_sample_2_relevance[cur_sample_to_add] = direct_diff
+            added_sample_2_pt_results[cur_sample_to_add] = (pt_best_entity_score, pt_target_entity_score, pt_target_entity_rank)
+
+        sorted_samples_with_relevance = sorted(added_sample_2_relevance.items(), key=lambda element: element[-1], reverse=True)
+
+        return sorted_samples_with_relevance, \
+               added_sample_2_pt_results, \
+               original_best_entity_score, original_target_entity_score, original_target_entity_rank, \
+               base_pt_best_entity_score, base_pt_target_entity_score, base_pt_target_entity_rank
+
+    def nple_addition_explanations(self,
+                    sample_to_explain: Tuple[Any, Any, Any],
+                    perspective: str,
+                    samples_to_add: list,
+                                   n: int):
+
+        # if there are less than n items in samples_to_add
+        # it is not possible to try combinations of length n of samples_to_add
+        if len(samples_to_add) < n:
+            return []
 
         # the behaviour of the engine must be deterministic!
         seed = 42
@@ -114,9 +189,8 @@ class PostTrainingEngine(ExplanationEngine):
         torch.cuda.set_rng_state(torch.cuda.get_rng_state())
         torch.backends.cudnn.deterministic = True
 
-        # sample to explain as a numpy array of the ids
+        # identify the entity to explain
         head_id, relation_id, tail_id = sample_to_explain
-        # name and id of the entity to explain, based on the passed perspective
         original_entity_id = head_id if perspective == "head" else tail_id
 
         # create a Kelpie Dataset focused on the original id of the entity to explain
@@ -127,41 +201,46 @@ class PostTrainingEngine(ExplanationEngine):
                                              kelpie_train_samples=kelpie_dataset.kelpie_train_samples) # type: KelpieModel
 
         # check how the post-trained "clone" performs on the sample to explain
-        (direct_score, inverse_score), _, _ = post_trained_model.predict_sample(sample=numpy.array(sample_to_explain),
-                                                                                original_mode=True)
-        results = []
+        all_scores = post_trained_model.all_scores(numpy.array([sample_to_explain])).detach().cpu().numpy()[0]
+        target_entity_score = all_scores[tail_id]
+        filter_out = self.dataset.to_filter[(head_id, relation_id)]
+        all_scores[filter_out] = -1e6  # todo: make it huge if the model wants to minimize scores
+        best_entity_score = numpy.max(all_scores)  # todo: min if the model wants to minimize scores
 
-        for i in range(len(samples_to_add)):
-            cur_sample_to_add = samples_to_add[i]
-            print("### ITER %i" % i)
-            print("### ADDED SAMPLE: ")
-            print("\t" + ";".join([str(cur_sample_to_add[0]),
-                                   str(cur_sample_to_add[1]),
-                                   str(cur_sample_to_add[2])]))
-            print("\t" + ";".join((kelpie_dataset.entity_id_2_name[int(cur_sample_to_add[0])],
-                                   kelpie_dataset.relation_id_2_name[int(cur_sample_to_add[1])],
-                                   kelpie_dataset.entity_id_2_name[int(cur_sample_to_add[2])])))
 
-            # create a Kelpie Dataset ... that also contains the fact to add
+        # extract all n-long different combinations of samples
+        samples_nples = self._extract_sample_nples(samples=samples_to_add, n=n)
+
+        added_nple_2_perturbed_score = {}
+        added_nple_2_relevance = {}
+
+        for i in range(len(samples_nples)):
+            current_nple_to_add = samples_nples[i]
+
+            # create a Kelpie Dataset ... that also contains the facts in the nple to add
             cur_kelpie_dataset = copy.deepcopy(kelpie_dataset)
 
-            cur_sample_to_add_numpy_arr = numpy.array([cur_sample_to_add])
-            cur_kelpie_dataset.add_training_samples(cur_sample_to_add_numpy_arr)
+            cur_nple_to_add_numpy_arr = numpy.array(current_nple_to_add)
+            cur_kelpie_dataset.add_training_samples(cur_nple_to_add_numpy_arr)
 
             cur_kelpie_model = self.post_train(kelpie_dataset=kelpie_dataset,
-                                                 kelpie_train_samples=cur_kelpie_dataset.kelpie_train_samples)  # type: KelpieModel
+                                               kelpie_train_samples=cur_kelpie_dataset.kelpie_train_samples)  # type: KelpieModel
 
-            (cur_direct_score, cur_inverse_score), \
-            (cur_head_rank, cur_tail_rank), _ = self.test_kelpie_model_on_sample(model=cur_kelpie_model,
+            (cur_direct_score, cur_inverse_score), _, _ = self.test_kelpie_model_on_sample(model=cur_kelpie_model,
                                                                                  sample=numpy.array(sample_to_explain))
-            direct_diff = direct_score - cur_direct_score
-            inverse_diff = inverse_score - cur_inverse_score
 
-            results.append((cur_sample_to_add, cur_direct_score, cur_inverse_score, cur_head_rank, cur_tail_rank, direct_diff + inverse_diff))
+            # TODO: this should depend on maximize/minimize
+            direct_diff = cur_direct_score - target_entity_score
 
-        results = sorted(results, key=lambda element: element[-1], reverse=True)
+            added_nple_2_relevance[current_nple_to_add] = direct_diff
+            added_nple_2_perturbed_score[current_nple_to_add] = cur_direct_score
 
-        return results
+        sorted_nples_with_relevance = sorted(added_nple_2_relevance.items(), key=lambda element: element[-1], reverse=True)
+
+        return sorted_nples_with_relevance, \
+               added_nple_2_perturbed_score, \
+               target_entity_score, \
+               best_entity_score
 
 
     def post_train(self,
@@ -173,12 +252,11 @@ class PostTrainingEngine(ExplanationEngine):
         :param kelpie_train_samples:
         :return:
         """
-        print("Post-training the Kelpie model...")
         kelpie_model_class = self.model.kelpie_model_class()
         kelpie_model = kelpie_model_class(model=self.model, dataset=kelpie_dataset)
         kelpie_model.to('cuda')
 
-        optimizer = self.kelpie_optimizer_class(model=kelpie_model, hyperparameters=self.hyperparameters)
+        optimizer = self.kelpie_optimizer_class(model=kelpie_model, hyperparameters=self.hyperparameters, verbose=False)
         optimizer.train(train_samples=kelpie_train_samples)
         return kelpie_model
 
@@ -197,55 +275,20 @@ class PostTrainingEngine(ExplanationEngine):
         return scores, ranks, predictions
 
 
-    def test_kelpie_model(self,
-                          testing_model: KelpieModel,
-                          sample_to_explain: numpy.array,
-                          perspective: str):
-        """
+    def extract_detailed_performances_on_sample(self,
+                                                model: Model,
+                                                sample: numpy.array):
+        model.eval()
+        head_id, relation_id, tail_id = sample
+        # check how the post-trained "clone" performs on the sample to explain
+        all_scores = model.all_scores(numpy.array([sample])).detach().cpu().numpy()[0]
+        target_entity_score = all_scores[tail_id] # todo: this only works in "head" perspective
+        filter_out = model.dataset.to_filter[(head_id, relation_id)]
+        all_scores[filter_out] = -1e6  # todo: make it huge if the model wants to minimize scores
+        best_entity_score = numpy.max(all_scores)  # todo: min if the model wants to minimize scores
 
-        :param testing_model:
-        :param sample_to_explain:
-        :param perspective:
-        :return:
-        """
-        testing_model.eval()
-        kelpie_dataset = testing_model.dataset   # type: KelpieDataset
+        # todo: MIN POLICY HERE
+        # todo: <= if the model wants to minimize scores
+        target_entity_rank = numpy.sum(all_scores >= target_entity_score)
 
-        # Kelpie model on original fact
-        scores, ranks, predictions = testing_model.predict_sample(sample=sample_to_explain, original_mode=True)
-        original_direct_score, original_inverse_score = scores[0], scores[1]
-        original_head_rank, original_tail_rank = ranks[0], ranks[1]
-        print("\nKelpie model on the original fact <%s, %s, %s>" % (kelpie_dataset.entity_id_2_name[sample_to_explain[0]],
-                                                                    kelpie_dataset.relation_id_2_name[sample_to_explain[1]],
-                                                                    kelpie_dataset.entity_id_2_name[sample_to_explain[2]]))
-        print("\tDirect fact score: %f; Inverse fact score: %f" % (original_direct_score, original_inverse_score))
-        print("\tHead Rank: %f" % original_head_rank)
-        print("\tTail Rank: %f" % original_tail_rank)
-
-        # Kelpie model on all facts containing the original entity
-        print("\nKelpie model on all test facts containing the original entity:")
-        mrr, h1 = KelpieEvaluator(testing_model).eval(samples=kelpie_dataset.original_test_samples, original_mode=True)
-        print("\tMRR: %f\n\tH@1: %f" % (mrr, h1))
-
-        ### Evaluation on kelpie entity
-        kelpie_entity_id = kelpie_dataset.kelpie_entity_id
-        kelpie_sample = numpy.array((kelpie_entity_id, sample_to_explain[1], sample_to_explain[2])) if perspective == "head" \
-            else numpy.array((sample_to_explain[0], sample_to_explain[1], kelpie_entity_id))
-
-        # results on kelpie fact
-        scores, ranks, predictions = testing_model.predict_sample(sample=kelpie_sample, original_mode=False)
-        kelpie_direct_score, kelpie_inverse_score = scores[0], scores[1]
-        kelpie_head_rank, kelpie_tail_rank = ranks[0], ranks[1]
-        print("\nKelpie model on the Kelpie test fact: <%s, %s, %s>" % (kelpie_dataset.entity_id_2_name[kelpie_sample[0]],
-                                                                        kelpie_dataset.relation_id_2_name[kelpie_sample[1]],
-                                                                        kelpie_dataset.entity_id_2_name[kelpie_sample[2]]))
-        print("\tDirect fact score: %f; Inverse fact score: %f" % (kelpie_direct_score, kelpie_inverse_score))
-        print("\tHead Rank: %f" % kelpie_head_rank)
-        print("\tTail Rank: %f" % kelpie_tail_rank)
-
-        # results on all facts containing the kelpie entity
-        print("\nKelpie model on all test facts containing the Kelpie entity:")
-        mrr, h1 = KelpieEvaluator(testing_model).eval(samples=kelpie_dataset.kelpie_test_samples, original_mode=False)
-        print("\tMRR: %f\n\tH@1: %f" % (mrr, h1))
-
-        return kelpie_direct_score, kelpie_inverse_score, kelpie_head_rank, kelpie_tail_rank
+        return target_entity_score, best_entity_score, target_entity_rank
