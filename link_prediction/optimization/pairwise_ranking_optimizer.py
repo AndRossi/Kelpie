27a55308d5f38500458170db908bdd17c52ef46d
Andrea Rossi <andrea@MBP-di-Andrea.lan>
2021-02-05 12:12:39 +0100

    simplified pairwise ranking optimizer to make Transe training faster

--- link_prediction/optimization/pairwise_ranking_optimizer.py
+++ link_prediction/optimization/pairwise_ranking_optimizer.py
@@ -2,17 +2,15 @@ import tqdm
 import torch
 import numpy as np
 from torch import optim
-from link_prediction.optimization.negative_sampling import CorruptionEngine
 from link_prediction.regularization.regularizers import L2
 from model import Model, BATCH_SIZE, LEARNING_RATE, EPOCHS, MARGIN, NEGATIVE_SAMPLES_RATIO, \
-    REGULARIZER_WEIGHT
+    REGULARIZER_WEIGHT, KelpieModel
 from optimizer import Optimizer
 
 
 class PairwiseRankingOptimizer(Optimizer):
     """
         This optimizer relies on Pairwise Ranking loss, also called Margin-based loss.
-
     """
 
     def __init__(self,
@@ -20,7 +18,7 @@ class PairwiseRankingOptimizer(Optimizer):
                  hyperparameters: dict,
                  verbose: bool = True):
         """
-            PairwiseRankingOpti mizer initializer.
+            PairwiseRankingOptimizer initializer.
             :param model: the model to train
             :param hyperparameters: a dict with the optimization hyperparameters. It must contain at least:
                     - BATCH SIZE
@@ -40,7 +38,7 @@ class PairwiseRankingOptimizer(Optimizer):
         self.negative_samples_ratio = hyperparameters[NEGATIVE_SAMPLES_RATIO]
         self.regularizer_weight = hyperparameters[REGULARIZER_WEIGHT]
 
-        self.corruption_engine = CorruptionEngine(self.dataset)
+        #self.corruption_engine = CorruptionEngine(self.dataset)
         self.loss = torch.nn.MarginRankingLoss(margin=self.margin, reduction="mean").cuda()
 
         self.optimizer = optim.Adam(params=self.model.parameters(), lr=self.learning_rate)  # we only support ADAM for PairwiseRankingOptimizer
@@ -52,13 +50,12 @@ class PairwiseRankingOptimizer(Optimizer):
               evaluate_every:int =-1,
               valid_samples:np.array = None):
 
-        print("Training the " + self.model.name + " model...")
-
         training_samples = np.vstack((train_samples, self.dataset.invert_samples(train_samples)))
 
         self.model.cuda()
 
         for e in range(1, self.epochs+1):
+
             self.epoch(training_samples=training_samples, batch_size=self.batch_size)
 
             if evaluate_every > 0 and valid_samples is not None and e % evaluate_every == 0:
@@ -80,36 +77,39 @@ class PairwiseRankingOptimizer(Optimizer):
             torch.save(self.model.state_dict(), save_path)
             print("\t done.")
 
+
     def epoch(self,
               batch_size: int,
               training_samples: np.array):
 
         np.random.shuffle(training_samples)
+        repeated = np.repeat(training_samples, self.negative_samples_ratio, axis=0)
+
+        all_positive_samples = torch.from_numpy(repeated)
+
+        head_or_tail = torch.randint(high=2, size=torch.Size([len(all_positive_samples)]))
+        random_entities = torch.randint(high=self.dataset.num_entities, size=torch.Size([len(all_positive_samples)]))
+
+        corrupted_heads = torch.where(head_or_tail == 1, random_entities, all_positive_samples[:, 0].type(torch.LongTensor))
+        corrupted_tails = torch.where(head_or_tail == 0, random_entities, all_positive_samples[:, 2].type(torch.LongTensor))
+        all_negative_samples = torch.stack((corrupted_heads, all_positive_samples[:, 1], corrupted_tails), dim=1)
+
+        all_positive_samples.cuda()
+        all_negative_samples.cuda()
         self.model.train()
 
         with tqdm.tqdm(total=len(training_samples), unit='ex', disable=not self.verbose) as bar:
-            bar.set_description(f'train loss')
+            bar.set_description('train loss')
             batch_start = 0
 
             while batch_start < len(training_samples):
-                positive_batch, negative_batch = self.extract_batch(training_samples=training_samples,
-                                                                    batch_start=batch_start,
-                                                                    batch_size=batch_size)
+                positive_batch, negative_batch = all_positive_samples[batch_start: min(batch_start+batch_size, len(training_samples))], \
+                                                 all_negative_samples[batch_start: min(batch_start+batch_size, len(training_samples))]
 
                 l = self.step_on_batch(positive_batch, negative_batch)
                 batch_start+=batch_size
                 bar.update(batch_size)
                 bar.set_postfix(loss=str(round(l.item(), 6)))
-
-            #if self.decay:
-            #    self.scheduler.step()
-
-    def extract_batch(self, training_samples, batch_start, batch_size):
-        original_batch = training_samples[batch_start: min(batch_start+batch_size, len(training_samples))]
-        positive_batch, negative_batch = self.corruption_engine.corrupt_samples(original_batch, self.negative_samples_ratio)
-
-        return torch.from_numpy(positive_batch).cuda(), torch.from_numpy(negative_batch).cuda()
-
 
     def step_on_batch(self, positive_batch, negative_batch):
 
@@ -130,18 +130,16 @@ class PairwiseRankingOptimizer(Optimizer):
 
 class KelpiePairwiseRankingOptimizer(PairwiseRankingOptimizer):
     def __init__(self,
-                 model:Model,
+                 model:KelpieModel,
                  hyperparameters: dict,
                  verbose: bool = True):
 
         super(KelpiePairwiseRankingOptimizer, self).__init__(model=model,
                                                              hyperparameters=hyperparameters,
                                                              verbose=verbose)
-
-        self.optimizer = optim.SGD(params=self.model.parameters(), lr=hyperparameters[LEARNING_RATE])
+        self.kelpie_entity_id = model.kelpie_entity_id
 
     # Override
-
     def epoch(self,
               batch_size: int,
               training_samples: np.array):
@@ -149,15 +147,31 @@ class KelpiePairwiseRankingOptimizer(PairwiseRankingOptimizer):
         np.random.shuffle(training_samples)
         self.model.train()
 
+        repeated = np.repeat(training_samples, self.negative_samples_ratio, axis=0)
+        all_positive_samples = torch.from_numpy(repeated)
+
+        random_entities = torch.randint(high=self.dataset.num_entities, size=torch.Size([len(all_positive_samples)]))
+        head_or_tail = torch.randint(high=2, size=torch.Size([len(all_positive_samples)]))
+        corrupted_heads = torch.where(head_or_tail == 1, random_entities, all_positive_samples[:, 0].type(torch.LongTensor))
+        corrupted_tails = torch.where(head_or_tail == 0, random_entities, all_positive_samples[:, 2].type(torch.LongTensor))
+
+        # kelpie_entity_is_head = torch.where(all_positive_samples[:, 0] == self.kelpie_entity_id, torch.ones(len(all_positive_samples)), torch.zeros(len(all_positive_samples)))
+        # corrupted_heads = torch.where(kelpie_entity_is_head == 0, random_entities, all_positive_samples[:, 0].type(torch.LongTensor))
+        # corrupted_tails = torch.where(kelpie_entity_is_head == 1, random_entities, all_positive_samples[:, 2].type(torch.LongTensor))
+
+        all_negative_samples = torch.stack((corrupted_heads, all_positive_samples[:, 1], corrupted_tails), dim=1)
+
+        all_positive_samples.cuda()
+        all_negative_samples.cuda()
+
         with tqdm.tqdm(total=len(training_samples), unit='ex', disable=not self.verbose) as bar:
-            bar.set_description(f'train loss')
+            bar.set_description('train loss')
 
             batch_start = 0
 
             while batch_start < len(training_samples):
-                positive_batch, negative_batch = self.extract_batch(training_samples=training_samples,
-                                                                    batch_start=batch_start,
-                                                                    batch_size=batch_size)
+                positive_batch, negative_batch = all_positive_samples[batch_start: min(batch_start+batch_size, len(training_samples))], \
+                                                 all_negative_samples[batch_start: min(batch_start+batch_size, len(training_samples))]
 
                 l = self.step_on_batch(positive_batch, negative_batch)
 
@@ -165,7 +179,6 @@ class KelpiePairwiseRankingOptimizer(PairwiseRankingOptimizer):
                 # THIS IS EXTREMELY IMPORTANT BECAUSE THIS WILL PROPAGATE THE UPDATES IN THE KELPIE ENTITY EMBEDDING
                 # TO THE MATRIX CONTAINING ALL THE EMBEDDINGS
                 self.model.update_embeddings()
-
 
                 batch_start+=batch_size
                 bar.update(batch_size)
